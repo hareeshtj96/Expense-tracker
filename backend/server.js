eca7096348c9222edcd66d0a3bb681f310a29474
Hareesh T J <evirexsoft@gmail.com>
2025-11-19 22:28:06 +0530

    changes in cors

--- backend/server.js
+++ backend/server.js
@@ -14,21 +14,20 @@ const PORT = process.env.PORT || 6000;
 const allowedOrigins = process.env.CLIENT_URLS.split(",");
 
 // Middlewares
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
 
-            if (allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS: " + origin));
-            }
-        },
-        credentials: true,
-    })
-);
-app.options("*", cors());
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+        return callback(new Error("Not allowed by CORS: " + origin));
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
